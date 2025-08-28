2d3e16727a0db78f7672e40131cb25e5d22ede19
Francesco Illuminati <fillumina@gmail.com>
2025-08-28 18:06:15 +0200

    add a timeout after the player fall and then restart
    Use a Timer node in Killzone to set the timeout
    and then in the signal handler restart the scene.

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,5 +1,18 @@
 extends Area2D
 
+# create this line by dragging Timer from the scene panel pressing CTRL
+# this is a reference to the node useful because the $xx notation is a 
+# a shortcut for a search function that runs at any call and thus is
+# not very efficient.
+@onready var timer: Timer = $Timer
+
 
 func _on_body_entered(body: Node2D) -> void:
 	print("you died")
+	timer.start()
+
+
+# runs when the timer ends
+func _on_timer_timeout() -> void:
+	# restart the game
+	get_tree().reload_current_scene()
