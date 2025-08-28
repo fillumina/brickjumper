1d2a1a614cb3abfa127860090b90975eedb0d029
Francesco Illuminati <fillumina@gmail.com>
2025-08-28 19:56:18 +0200

    add movements to the slime bounching between 2 blocks

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,12 +1,34 @@
 extends Node2D
 
+# speed in FPS
+const SPEED := 60
+
+# direction is -1 when it goes backwards and 1 if it goes forward
+var direction := 1
+
+
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# start the animation here instead of setting autostart so the editor
 	# won't persist its last frame
-	$AnimatedSprite2D.play()
+	animated_sprite.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+# To move the slime it could have been used the animation but this way is
+# more generic because the slime can be put everywhere in the scene 
+# between two hard blocks and it would go right and left all by itself!
 func _process(delta: float) -> void:
-	pass
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = true
+	
+	if ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = false
+	
+	position.x += direction * SPEED * delta
