425d6a091891157662ebe92556235f8585b2a13f
Francesco Illuminati <fillumina@gmail.com>
2025-08-28 17:37:48 +0200

    create the coin scene and add it to the game

---
+++ scripts/coin.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# starts the coin animation to avoid having the editor autostart it
+	# and persist its last position in the scene cluttering the versioning
+	$AnimatedSprite2D.play()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
