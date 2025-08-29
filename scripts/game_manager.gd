65b09c4cf12bd84f444116e782b4068fd53b56d6
Francesco Illuminati <fillumina@gmail.com>
2025-08-29 09:14:45 +0200

    add a score label

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,5 +1,6 @@
 extends Node
 
+@onready var score_label: Label = $ScoreLabel
 
 # the := means that the score is boud to the type of the given value
 var score := 0
@@ -7,4 +8,4 @@ var score := 0
 
 func add_point():
 	score += 1
-	print(score)
+	score_label.text = "You collected " + str(score) + " coins."
