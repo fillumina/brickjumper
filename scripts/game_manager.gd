272640335b44cdeacec7afcaf202e0d0aefee73f
Francesco Illuminati <fillumina@gmail.com>
2025-08-29 20:38:23 +0200

    add jump sound and won/lost label

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,11 +1,23 @@
 extends Node
 
+
 @onready var score_label: Label = $ScoreLabel
+@onready var win_label: Label = $WinLabel
 
 # the := means that the score is boud to the type of the given value
 var score := 0
+var win_score: int
 
+func _ready():
+	# initialize the win_score to the total amount of coins
+	win_score = get_parent().get_node("Coins").get_child_count()
+	
 
 func add_point():
 	score += 1
 	score_label.text = "You collected " + str(score) + " coins."
+	
+	if score == win_score:
+		win_label.text = "YOU WON!"
+	else:
+		win_label.text = "YOU LOST!"
--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,7 @@ const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var audio_stream_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 
 func _ready():
@@ -23,6 +24,8 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		if not audio_stream_player.playing:
+			audio_stream_player.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
