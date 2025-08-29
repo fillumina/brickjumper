7c17868ecbab1a85a61d2ebdc2223f0de32ffff4
Francesco Illuminati <fillumina@gmail.com>
2025-08-29 20:15:58 +0200

    add falling sound on kill

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -5,16 +5,17 @@ extends Area2D
 # a shortcut for a search function that runs at any call and thus is
 # not very efficient.
 @onready var timer: Timer = $Timer
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _on_body_entered(body: Node2D) -> void:
-	print("you died")
 	# modify the game timescale so the game will slow down
 	Engine.time_scale = 0.5
 	# the body is the node the killzone collided with so in this case is the player
 	# so we search for the player's node collision shape and remove it making
 	# the player falling down with dramatic effect (and slowly because of the
 	# time scale).
+	animation_player.play("falling")
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
